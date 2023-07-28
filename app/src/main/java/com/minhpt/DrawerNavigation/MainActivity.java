2a031837603dbd48846e4c18cd6699d660733fce
Minh <phamminh20804@gmail.com>
2023-07-28 16:43:11 +0700

    Commit

--- app/src/main/java/com/minhpt/DrawerNavigation/MainActivity.java
+++ app/src/main/java/com/minhpt/DrawerNavigation/MainActivity.java
@@ -12,7 +12,7 @@ import android.view.MenuItem;
 import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
-        private DrawerLayout drawerLayout;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
